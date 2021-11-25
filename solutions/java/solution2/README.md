ea41e89ecddd07c637fce1f6acf9c9d2d67aa496
sonias1 <78326088+sonias1@users.noreply.github.com>
2021-11-25 18:08:39 +0000

    Delete solutions directory

--- solutions/java/solution2/README.md
+++
@@ -1,1 +0,0 @@
-This solution contains code for Part 1 and Part 2 of the challenge.
--- solutions/python/solution2/video_playlist.py
+++
@@ -1,9 +0,0 @@
-"""A video playlist class."""
-
-
-class Playlist:
-    """A class used to represent a Playlist."""
-
-    def __init__(self, name):
-        self.name = name
-        self.videos = []
